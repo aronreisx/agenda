3064c56674807583a43e6a5f54cfa998ee985328
Aron Reis <aronreis2@gmail.com>
2021-08-20 22:18:06 +0100

    feat: Setted MongoDB connection

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-const port = 3333;
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS
+}).then(() => {
+    app.emit('connected'),
+    console.log('Database connected successfully');
+}).catch((err) => {
+    console.log(err);
+});
 
-app.listen(port, () => {
-    console.log(`App is running at http://localhost:${port}`);
+app.on('connected', () => {
+    const port = 3333;
+
+    app.listen(port, () => {
+        console.log(`App is running at http://localhost:${port}`);
+    });
 });
