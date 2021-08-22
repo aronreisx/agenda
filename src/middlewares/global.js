817c1acdded86e8cd6d507621dde1f4cfd0bcced
Aron Reis <aronreis2@gmail.com>
2021-08-22 17:44:59 +0100

    feat: Creating csrfToken middleware

--- src/middlewares/global.js
+++ src/middlewares/global.js
@@ -3,3 +3,8 @@ export default checkCsrfError = (err, req, res, next) => {
     return res.status(403).render('403');
   }
 }
+
+export default csrfToken = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
