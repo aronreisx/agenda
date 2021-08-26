8cd5252ee96001f2c63bf2873177d3c6865c55b2
Aron Reis <aronreis2@gmail.com>
2021-08-26 18:58:09 +0100

    style: line order modified

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const route = express.Router();
-const home = require('./src/controllers/home');
 
-route.get('/', home.home);
+const root = require('./src/controllers/rootController');
+const login = require('./src/controllers/loginController');
+
+route.get('/', root.index);
+
+route.get('/login/index', login.index);
+route.post('/login/register', login.register);
 
 module.exports = route;
