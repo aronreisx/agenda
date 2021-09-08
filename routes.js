8cbebe586477809386864ca84ed2c5a6e004c27d
Aron Reis <aronreis2@gmail.com>
2021-09-08 13:18:00 +0100

    feat: routes added to routes.js

--- routes.js
+++ routes.js
@@ -3,10 +3,24 @@ const route = express.Router();
 
 const root = require('./src/controllers/rootController');
 const login = require('./src/controllers/loginController');
+const contact = require('./src/controllers/contactController');
 
+const {loginRequired} = require('./src/middlewares/loginRequired');
+
+//* Route Home
 route.get('/', root.index);
 
+//* Routes Login
 route.get('/login/index', login.index);
 route.post('/login/register', login.register);
+route.post('/login/login', login.login);
+route.get('/login/logout', login.logout);
+
+//* Routes Contact
+route.get('/contact/index', loginRequired, contact.index);
+route.post('/contact/register', loginRequired, contact.register);
+route.get('/contact/index/:id', loginRequired, contact.editIndex);
+route.post('/contact/edit/:id', loginRequired, contact.edit)
+route.get('/contact/delete/:id', loginRequired, contact.delete)
 
 module.exports = route;
