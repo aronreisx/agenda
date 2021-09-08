ac0c320f10abc0b3ac3645fdf1b8c9830ab62b2d
Aron Reis <aronreis2@gmail.com>
2021-09-08 13:25:24 +0100

    feat: login model new methods added

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
-const loginSchema = new Schema({
+const loginSchema = new mongoose.Schema({
   email: { type: String, require: true },
   password: { type: String, require: true },
-  token: { type: String, require: true },
-  createdAt: {type: Date, default: Date.now}
 });
 
 const LoginModel = mongoose.model('Login', loginSchema);
@@ -17,15 +16,41 @@ class Login {
     this.user = null;
   }
 
+  async login(){
+    this.validate();
+    if(this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({email:this.body.email});
+
+    if(!this.user) {
+        this.errors.push('Invalid user or password');
+        return
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+        this.errors.push('Invalid user or password');
+        this.user = null;
+        return
+    }
+  }
+
   async register(){
     this.validate();
     if(this.errors.length > 0) return;
 
-    try{
-      this.user = await LoginModel.create(this.body);
-    } catch(err) {
-      console.log(err);
-    }
+    await this.userExists();
+    if(this.errors.length > 0) return;
+
+    //* USING PASSOWRD HASHING AFTER VALIDATION AND CLEANING UP
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+    this.user = await LoginModel.create(this.body);
+  }
+
+  async userExists(){
+    const user = await LoginModel.findOne({email:this.body.email})
+    if(user) this.errors.push('The user already exists.')
   }
 
   validate(){
@@ -33,12 +58,12 @@ class Login {
 
     //_ validation
     //_ email needs to be valid // OR push an error to Errors array
-    if(!validator.isEmail(this.body.email)) this.errors.push('Email is not valid');
+    if(!validator.isEmail(this.body.email)) this.errors.push('The email you provided is not valid.');
 
     //_password needs to have between 3 and 50 characters
     if(this.body.password.length < 3 || this.body.password.length > 50){
       //_ send message to user about password error
-      this.errors.push('Password needs to have between 3 and 50 characters');
+      this.errors.push('The  password needs to have between 3 and 50 character.');
     }
   }
 
@@ -51,7 +76,9 @@ class Login {
 
     this.body = {
       email: this.body.email,
-      password: this.body.password
+      password: this.body.password,
     }
   }
 }
+
+module.exports = Login;
