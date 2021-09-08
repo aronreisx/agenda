de470a4ddaf1684874a829b8f478cb29631b1bef
Aron Reis <aronreis2@gmail.com>
2021-09-08 13:30:09 +0100

    feat: new methods added to rootController

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,3 +1,7 @@
-exports.index = (req, res) => {
-    res.render('index');
+const Contact = require('../models/contactModel');
+
+exports.index = async (req, res) => {
+  if(!req.session.user) return res.render('index', { contacts: {} });
+  const contacts = await Contact.searchContacts(req.session.user.email);
+  res.render('index', { contacts });
 };
